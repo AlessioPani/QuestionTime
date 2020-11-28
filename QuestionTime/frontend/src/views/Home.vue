1b35837c49b185e2e52576258d03bdd327a049e7
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2020-11-28 18:39:55 +0100

    Added apiService function and modified index page - Index page now retrieve all the questions made - Added style using Bootstrap

--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,20 +1,68 @@
 <template>
-  <div class="home">
-    <div class="container">
-      <img alt="Vue logo" src="../assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
+<div class="home-view">
+    <div class="container"> 
+        <div class="mt-2">
+        	<div v-for="question in questions" :key="question.pk">
+				<div class="card border-secondary mt-2">
+					<div class="card-header">
+						Question submitted by 
+						<span class="author-name">{{ question.author }}</span>
+						on
+						<span class="created-date">{{ question.created_at }}</span>
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">{{ question.content }}</h5>
+						<hr class="mb-1 mt-1">
+						<p class="card-text answers-count">Answers: {{ question.answers_count}}</p>
+					</div>
+				</div>
+			</div>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { apiService } from "../common/api.service"
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      questions: []
+    }
+  },
+
+  methods: {
+    getQuestions() {
+      let endpoint = "/api/questions/";
+      apiService(endpoint)
+        .then(data => {
+          this.questions.push(...data.results)
+        })
+    }
+  },
+
+  created() {
+    this.getQuestions();
   }
 };
 </script>
+
+<style media="screen">
+	.author-name {
+		font-weight: bold;
+		color: #DC3545;
+	}
+
+	.answers-count {
+		font-size: 80%;
+	}
+
+	.created-date {
+		font-weight: bold;
+		color: #DC3545;
+	}
+</style>
+
