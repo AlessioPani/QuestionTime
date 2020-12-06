60dc982d8037a4d7acc32d98703389e0b2cebec7
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2020-12-06 16:13:05 +0100

    Added some changes for the production build - Tested successfully

--- QuestionTime/core/views.py
+++ QuestionTime/core/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+from django.conf import settings
 
 
 class indexTemplateView(LoginRequiredMixin, TemplateView):
@@ -7,5 +8,8 @@ class indexTemplateView(LoginRequiredMixin, TemplateView):
     View used to render the index.html template.
     '''
     def get_template_names(self):
-        template_name = "index.html"
+        if settings.DEBUG:
+            template_name = "index-dev.html"
+        else:
+            template_name = "index.html"
         return template_name
